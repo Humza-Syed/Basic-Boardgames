58ad9f95f20b081ef4d5635c8064c5feec19912c
Humza <humz_man@hotmail.com>
2019-12-24 15:35:45 +0000

    Header files. Universal and tic_tac_toe

---
+++ TicTacToe/tic_tac_toe.h
@@ -0,0 +1,10 @@
+//
+// Created by Humza on 24/12/2019.
+//
+
+#ifndef BASIC_BOARDGAMES_TIC_TAC_TOE_H
+#define BASIC_BOARDGAMES_TIC_TAC_TOE_H
+
+#endif //BASIC_BOARDGAMES_TIC_TAC_TOE_H
+
+void play_tic_tac_toe();
