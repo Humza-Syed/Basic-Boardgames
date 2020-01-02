75d23a6e7249037ed03b18a3b5006bd1defe1d1e
Humza <humz_man@hotmail.com>
2020-01-02 14:18:37 +0000

    Created swap_type_array

--- TicTacToe/tic_tac_toe.c
+++ TicTacToe/tic_tac_toe.c
@@ -1,24 +1,50 @@
 #include "tic_tac_toe.h"
 
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 void play_tic_tac_toe(struct player players[]){
     char board[BOARD_SIZE][BOARD_SIZE];
+    char play_type[] = {'X','O'};
     int number_of_games = set_number_of_games();
     int games_played = 0;
+
     gameStatus = CONTINUE;
 
+    int players_turn = select_side(players);
+    if(players_turn == 1){
+        swap_type_array(play_type);
+    }
+
     while(games_played < number_of_games){
+        initialise_board(board);
         while(gameStatus == CONTINUE){
-            initialise_board(board);
             print_board(board);
+            make_move(board,players_turn);
         }
         games_played++;
     }
 
+}
+
+int set_number_of_games(){
+    int number_of_games;
+    char input;
+    printf("Please enter the number of games you wish to play\n");
+    fgets(&input,2,stdin);
+    fflush(stdin);
+    number_of_games = strtol(&input,NULL,10);
+    return number_of_games;
+}
+
+int select_side(struct player players[]){
+    int decision = 0;
+    char input_number;
+    printf("Please enter which player will start as 'X'.\nPlayers will swap in each successive game\nPlease enter the corresponding number\n1.%s\n2.%s",players[0].player_name,players[1].player_name);
+    while(decision < 1 || decision > 2){
+        fgets(&input_number,2,stdin);
+        fflush(stdin);
+        decision = strtol(&input_number,NULL,10);
+    }
+
+    return --decision;
 }
 
 void initialise_board(char board[][BOARD_SIZE]){
@@ -39,9 +65,12 @@ void print_board(char board[][3]){
    }
 }
 
-int set_number_of_games(){
-    int number_of_games;
-    printf("Please enter the number of games you wish to play\n");
-    scanf("%d",&number_of_games);
-    return number_of_games;
+void make_move(char board[][BOARD_SIZE], int which_player){
+
+}
+
+void swap_type_array(char type_array[]){
+    char temp = type_array[0];
+    type_array[0] = type_array[1];
+    type_array[1] = temp;
 }
--- TicTacToe/tic_tac_toe.h
+++ TicTacToe/tic_tac_toe.h
@@ -1,4 +1,7 @@
 #include "../universal_definitions.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #ifndef BASIC_BOARDGAMES_TIC_TAC_TOE_H
 #define BASIC_BOARDGAMES_TIC_TAC_TOE_H
@@ -11,4 +14,7 @@ enum type {X,Y};
 
 void initialise_board(char board[][BOARD_SIZE]);
 void print_board(char board[][BOARD_SIZE]);
-void move(int x,int j);
+void make_move(char board[][BOARD_SIZE], int which_player);
+int select_side(struct player players[]);
+void swap_sides();
+void swap_type_array(char type_array[]);
