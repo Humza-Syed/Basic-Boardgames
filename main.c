01b3537dcbe7d2310fac68222e5860997650c930
Humza <humz_man@hotmail.com>
2020-01-13 15:25:11 +0000

    Game now returns to gamelist interface after a game is finished.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "universal_definitions.h"
 
@@ -11,26 +12,29 @@ int main() {
     initialise_players(players);
 
     printf("Welcome to my program\nPlease select the corresponding game\n");
-    printf("Games List\n1. Tic-Tac-Toe\n2. Connect 4\n3. MasterMind\n4. Exit program\n");
 
-    while(desired_game <= 0 || desired_game > 4){
-        fgets(&input_number, 2, stdin);
-        fflush(stdin);
-        desired_game = strtol(&input_number, NULL, 10);
+    while(true){
+        do{
+            printf("Games List\n1. Tic-Tac-Toe\n2. Connect 4\n3. MasterMind\n4. Exit program\n");
+            fgets(&input_number, 2, stdin);
+            fflush(stdin);
+            desired_game = strtol(&input_number, NULL, 10);
+        }while(desired_game <= 0 || desired_game > 4);
+
+        switch(desired_game){
+            case(1) :
+                printf("Player wants to play:  \"Tic-Tac-Toe\"\n");
+                play_tic_tac_toe(players);
+                break;
+            case(2) :
+                printf("Player wants to play: \"Connect 4 \"\n");
+            case(3) :
+                printf("Player wants to play: \"Mastermind\"\n");
+            default :
+                exit(0);
+        }
     }
 
-    switch(desired_game){
-        case(1) :
-            printf("Player wants to play:  \"Tic-Tac-Toe\"\n");
-            play_tic_tac_toe(players);
-            break;
-        case(2) :
-            printf("Player wants to play: \"Connect 4 \"\n");
-        case(3) :
-            printf("Player wants to play: \"Mastermind\"\n");
-        default :
-            exit(0);
-    }
 }
 
 struct player initialise_players(struct player players[]){
